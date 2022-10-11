2af24481788e1db9a81c5617621f4ee18ccdb4cc
Vishal Das <dvishal485@gmail.com>
2022-10-11 14:45:14 +0530

    added program 07
    Signed-off-by: Vishal Das <dvishal485@gmail.com>

---
+++ programs/program_07.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+    string name;
+    int age;
+
+  public:
+    Person(string name, int age) : name(name), age(age) {}
+    void display() {
+        cout << "Person Name\t: " << name << endl;
+        cout << "Person Age\t: " << age << endl;
+    }
+};
+
+class Literate : public Person {
+    bool employmentStatus;
+
+  public:
+    Literate(string name, int age, bool is_employed)
+        : Person(name, age), employmentStatus(is_employed) {}
+    void display() {
+        Person::display();
+        cout << "Employed\t: " << (employmentStatus ? "Yes" : "No") << endl;
+    }
+};
+
+class Student : public Literate {
+    int roll;
+
+  public:
+    Student(string name, int age, bool is_employed, int roll)
+        : Literate(name, age, is_employed), roll(roll) {}
+    void display() {
+        Literate::display();
+        cout << "Roll Number\t: " << roll << endl;
+    }
+};
+
+class Staff : public Literate {
+    int staffId;
+
+  public:
+    Staff(string name, int age, bool is_employed, int staff_id)
+        : Literate(name, age, is_employed), staffId(staff_id) {}
+    void display() {
+        Literate::display();
+        cout << "Staff ID\t: " << staffId << endl;
+    }
+};
+
+int main(void) {
+    Student s1("John", 20, false, 527);
+    Staff s2("Jack", 22, true, 768);
+    Student s3("Sam", 21, true, 523);
+
+    s1.display();
+    cout << endl;
+    s2.display();
+    cout << endl;
+    s3.display();
+
+    return 0;
+}
